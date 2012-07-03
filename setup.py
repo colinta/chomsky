9fd5553835a35a8ff08e0d5d7cf0540171625359
Colin Thomas-Arnold <colinta@gmail.com>
2012-07-03 00:51:58 -0600

    v0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
         name="chomsky",
-        version="v0.0.2",
+        version="v0.0.3",
         author="Colin Thomas-Arnold",
         author_email="colinta@gmail",
         url="https://github.com/colinta/chomsky",
