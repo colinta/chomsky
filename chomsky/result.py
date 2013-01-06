db4f078ed2d54995d63b8944a69e599b1d82918e
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-06 13:26:35 -0700

    misc stuff in ResultList

--- chomsky/result.py
+++ chomsky/result.py
@@ -8,6 +8,9 @@ class Result(unicode):
         return Result(super(Result, self).__getitem__(key))
 
     def __getslice__(self, start, stop):
+        '''
+        deprecated, but unicode defines it, so have to override it here
+        '''
         return Result(super(Result, self).__getslice__(start, stop))
 
 
@@ -18,5 +21,13 @@ class ResultList(list):
     def __str__(self):
         return Result(''.join(str(result) for result in self))
 
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return ResultList(super(ResultList, self).__getitem__(key))
+        return super(ResultList, self).__getitem__(key)
+
     def __getslice__(self, start, stop):
+        '''
+        deprecated, but list defines it, so have to override it here
+        '''
         return ResultList(super(ResultList, self).__getslice__(start, stop))
