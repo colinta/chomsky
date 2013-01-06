a1d973229c1a313408d1d248188e9110951fc97c
Colin Thomas Arnold Gray <colinta@gmail.com>
2013-01-06 13:26:52 -0700

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
         name="chomsky",
-        version="v0.0.17",
+        version="v0.0.18",
         author="Colin Thomas-Arnold",
         author_email="colinta@gmail",
         url="https://github.com/colinta/chomsky",
